8a2f4843feb4e11401f6db78fd8266498876cb46
Unknowncode07 <95133378+Unknowncode07@users.noreply.github.com>
2023-09-29 15:31:30 +0530

    MongoDB setup

--- client/src/scenes/dashboard/Row1.tsx
+++ client/src/scenes/dashboard/Row1.tsx
@@ -1,7 +1,10 @@
 import DashboardBox from '@/components/DashboardBox'
+import { useGetKpisQuery } from '@/state/api'
 
 
 const Row1 = () => {
+    const { data } = useGetKpisQuery();
+
   return (
     <>
         <DashboardBox  gridArea='a'></DashboardBox>
